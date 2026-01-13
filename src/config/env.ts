25a2bfd2cc6a365dcfcac805516162af84bb8bb8
Leandro Siqueira <le_kts12@hotmail.com>
2026-01-12 22:46:54 -0300

    feat: add Cognito configuration to environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,4 +2,6 @@ export const env = {
   MIVO_TABLE: process.env.MIVO_TABLE!,
   ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID!,
   ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY!,
+  COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID!,
+  COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID!,
 };
